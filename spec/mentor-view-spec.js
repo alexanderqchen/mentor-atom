8c2241be4ce3d4bbcb289e968232e531d0a4b9f1
alexanderqchen <aqchen@ucla.edu>
2019-02-23 17:20:59 -0700

    section highlighting by toggle

---
+++ spec/mentor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MentorView from '../lib/mentor-view';
+
+describe('MentorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	////
+		for i in hello:
+			print(i)
+
+		if asdf:
+			asdf
+		else:
+			ok i guess
+
+	////
+	fmt.Println("Hello, world.")
+}
